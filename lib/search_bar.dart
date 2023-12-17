e981afaaac92e489980e6f4eed3177d0a16c8a0a
hivx <chuvanhieu357@gmail.com>
2023-12-17 23:23:58 +0700

    update search_bar

--- lib/search_bar.dart
+++ lib/search_bar.dart
@@ -1,10 +1,7 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'network/network_request.dart';
-import 'models/search_post.dart';
 
 class SearchBarApp extends StatefulWidget {
-  const SearchBarApp({Key? key}) : super(key: key);
+  const SearchBarApp({super.key});
 
   @override
   State<SearchBarApp> createState() => _SearchBarAppState();
@@ -14,71 +11,30 @@ class _SearchBarAppState extends State<SearchBarApp> {
   final TextEditingController _searchController = TextEditingController();
   bool isSearchClicked = false;
   String searchText = '';
-  List<SearchPost>? searchPosts;
-  List<SearchPost>? searchResults;
+  List<String> items = [];
 
+  List<String> filteredItems = [];
   @override
   void initState() {
     super.initState();
-    // Gọi hàm để lấy dữ liệu từ API khi màn hình khởi tạo
-    fetchAndSetSearchPosts();
+    filteredItems = List.from(items);
   }
 
-  Future<void> fetchAndSetSearchPosts() async {
-    try {
-      final networkRequest = NetworkRequest();
-      // final posts = await networkRequest.fetchPosts();
-      // setState(() {
-      //   searchPosts = posts;
-      //   searchResults = List.from(posts);
-      // });
-    } catch (error) {
-      // Xử lý lỗi nếu có
-      if (kDebugMode) {
-        print("Error fetching data: $error");
-      }
-    }
-  }
-
-  void _onSearchChanged(String value) async {
+  void _onSearchChanged(String value) {
     setState(() {
       searchText = value;
+      myFilterItems();
     });
-
-    try {
-      final networkRequest = NetworkRequest();
-      final request = Request(keyword: searchText);
-      final posts = await networkRequest.fetchPosts(request);
-
-      setState(() {
-        searchPosts = posts;
-        searchResults = List.from(posts);
-      });
-    } catch (error) {
-      // Xử lý lỗi nếu có
-      if (kDebugMode) {
-        print("Error fetching data: $error");
-      }
-    }
   }
 
   void myFilterItems() {
     if (searchText.isEmpty) {
-      setState(() {
-        searchResults = List.from(searchPosts!);
-      });
+      filteredItems = List.from(items);
     } else {
-      setState(() {
-        searchResults = searchPosts!
-            .where((post) =>
-        post.data!
-            .any((data) =>
-            data.name!.toLowerCase().contains(searchText.toLowerCase())) ||
-            post.data!
-                .any((data) =>
-                data.described!.toLowerCase().contains(searchText.toLowerCase())))
-            .toList();
-      });
+      filteredItems = items
+          .where(
+              (item) => item.toLowerCase().contains(searchText.toLowerCase()))
+          .toList();
     }
   }
 
@@ -87,6 +43,8 @@ class _SearchBarAppState extends State<SearchBarApp> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
+        /* if the search button is clickable then show
+         the container otherwise text "Search Bar"*/
         title: isSearchClicked
             ? Container(
           height: 40,
@@ -98,11 +56,10 @@ class _SearchBarAppState extends State<SearchBarApp> {
             controller: _searchController,
             onChanged: _onSearchChanged,
             decoration: const InputDecoration(
-              contentPadding: EdgeInsets.fromLTRB(16, 20, 16, 12),
-              hintStyle: TextStyle(color: Colors.black),
-              border: InputBorder.none,
-              hintText: 'Tìm kiếm',
-            ),
+                contentPadding: EdgeInsets.fromLTRB(16, 20, 16, 12),
+                hintStyle: TextStyle(color: Colors.black),
+                border: InputBorder.none,
+                hintText: 'Search..'),
           ),
         )
             : const Text("Search Bar"),
@@ -121,19 +78,14 @@ class _SearchBarAppState extends State<SearchBarApp> {
           )
         ],
       ),
-      body: searchResults != null
-          ? ListView.builder(
-        itemCount: searchResults!.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(searchResults![index].data![0].name ?? ""),
-            // Hiển thị các thông tin khác theo nhu cầu
-          );
-        },
-      )
-          : const Center(
-        child: CircularProgressIndicator(),
-      ),
+      // body parts
+      body: ListView.builder(
+          itemCount: filteredItems.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(filteredItems[index]),
+            );
+          }),
     );
   }
 }
